6c0b4258060faf5ceae80422f94f47098cb92041
funnythingz <hiroki@hackleberry.me>
2014-06-25 16:35:50 +0900

    setup

--- src/ts/_simple-controller.ts
+++ src/ts/_simple-controller.ts
@@ -1,16 +1,11 @@
-/// <reference path="../../typings/tsd.d.ts" />
-
 module App {
 
     export interface ISimpleScope extends ng.IScope {
-        greeting: string;
     }
 
     export class SimpleController {
 
         constructor(private $scope: ISimpleScope) {
-
-            this.$scope.greeting = 'hello Angular !';
 
         }
 
